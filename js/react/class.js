9a4d94b431dc4c9d54a4d656e1cb8f2adcb1a4bd
hudamin <hdm258i@gmail.com>
2018-03-14 21:08:39 +0800

    master: add js/react/class.js

---
+++ js/react/class.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// for React 15 and below
+// import React, { PropTypes, Component } from 'react';
+
+export class KlassName extends Component {
+
+    /**
+     * Constructing
+     */
+    constructor( props ) {
+        super( props );
+    }
+
+
+
+
+    /****************************************
+     * Life Circles
+     */
+
+    /**
+     * Mounting Phase
+     */
+    componentWillMount() {}
+    componentDidMount() {}
+
+    /**
+     * Updating Phase
+     */
+    componentWillReceiveProps( nextProps ) {}
+    shouldComponentUpdate( nextProps, nextState ) { return true; }
+    componentWillUpdate( nextProps, nextState ) {}
+
+    /**
+     * Rendering 
+     */
+    render() {}
+
+    /**
+     * Unmounting Phase
+     */
+    componentWillUnmount() {}
+
+
+
+
+
+    /****************************************
+     * Event Handlers
+     */
+
+
+
+
+
+    /****************************************
+     * Other
+     */
+
+}
+
